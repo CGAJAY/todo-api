ce52acb187546514475b4ccaedf560da9f37b6e9
Mark Muthii <mark.muthii@strathmore.edu>
2024-09-05 15:24:15 +0300

    Seeders

--- src/database/Seeders/index.js
+++ src/database/Seeders/index.js
@@ -1,1 +1,18 @@
-// silence is golden
+import { Role } from "../Models/index.js";
+import { roles } from "./roles.js";
+
+export const seeders = {
+  up: async function () {
+    try {
+      await this.down(); // clear the database, so we don't have duplicate data for each time we run the server
+      await Role.insertMany(roles);
+      console.log("Database seeded successfully");
+    } catch (error) {
+      console.error("Database seeding failed", error);
+      process.exit(1);
+    }
+  },
+  down: async function () {
+    await Role.deleteMany();
+  },
+};
---
+++ src/database/Seeders/roles.js
@@ -0,0 +1,10 @@
+export const roles = [
+  {
+    name: "Admin",
+    slug: "admin",
+  },
+  {
+    name: "User",
+    slug: "user",
+  },
+];
