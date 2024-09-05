87a9f5649a7960dcfa4fe6b71965949ae20bd589
Mark Muthii <mark.muthii@strathmore.edu>
2024-09-05 15:20:59 +0300

    Update server.js, assets

--- src/server.js
+++ src/server.js
@@ -1,24 +1,36 @@
 import express from "express";
 import { homeRouter, tasksRouter } from "./routes/index.js";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import { configDotenv } from "dotenv";
+import { connectDB } from "./database/connect.js";
+import { seeders } from "./database/Seeders/index.js";
+
+configDotenv(); // Load environment variables
+
+await connectDB(); // connect to the database
+
+await seeders.up(); // seed the database
 
 const app = express();
 const PORT = process.env.PORT | 3005;
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
-/**
- * RouteS to the home page ("/", "/about", "/contact")
- */
+app.use(express.json()); // required to parse JSON bodies
+app.use(express.urlencoded({ extended: true })); // required to parse form data
+
+// required to use __dirname with ES6 modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.use("/assets", express.static(__dirname + "/assets/")); // serve static files (images, css, js) from the assets directory
+
+// Routes to the public pages ("/", "/about", "/contact")
 app.use("/", homeRouter);
 
-/**
- * RouteS to the tasks page ("/tasks", "/tasks/new", "/tasks/:id")
- */
+// Routes to the tasks pages ("/tasks", "/tasks/new", "/tasks/:id")
 app.use("/tasks", tasksRouter);
 
-/**
- * Route to handle 404 errors
- */
+// Middleware to handle all other routes (404)
 app.use("*", (req, res) => {
   res.status(404).json({ message: "Not Found" });
 });
